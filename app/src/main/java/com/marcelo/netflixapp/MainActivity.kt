fade2550b5e013c63beb5bf7829cf07b77e471a1
Marcelo Santos <rocha.mer21@gmail.com>
2022-09-22 18:00:15 -0300

    criado o progress bar e a interface dos resultados da chamada a api

--- app/src/main/java/com/marcelo/netflixapp/MainActivity.kt
+++ app/src/main/java/com/marcelo/netflixapp/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.marcelo.netflixapp
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.marcelo.netflixapp.model.Category
+import com.marcelo.netflixapp.util.CategoryTask
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CategoryTask.Callback {
+
+    private lateinit var progress: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progress = findViewById(R.id.progress_main)
 
         val categories = mutableListOf<Category>()
 
@@ -17,6 +27,24 @@ class MainActivity : AppCompatActivity() {
         val rv: RecyclerView = findViewById(R.id.rv_main)
         rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
+
+        CategoryTask(this).execute("https://api.tiagoaguiar.co/netflixapp/home?apiKey=1e213e5b-a13c-4b4e-a60e-c8880e176664")
+
+
+    }
+
+    override fun onPreExecute() {
+        progress.visibility = View.VISIBLE
+    }
+
+    override fun onResult(categories: List<Category>) {
+        Log.i("TAG", "onResult: $categories")
+        progress.visibility = View.GONE
+    }
+
+    override fun onFailure(message: String) {
+        progress.visibility = View.GONE
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
 }
