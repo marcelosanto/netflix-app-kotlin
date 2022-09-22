913207029bac4630a4227ff0cb648b95ed1f1cca
Marcelo Santos <rocha.mer21@gmail.com>
2022-09-22 19:50:20 -0300

    backbutton

--- app/src/main/java/com/marcelo/netflixapp/MovieActivity.kt
+++ app/src/main/java/com/marcelo/netflixapp/MovieActivity.kt
@@ -2,6 +2,7 @@ package com.marcelo.netflixapp
 
 import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -46,5 +47,13 @@ class MovieActivity : AppCompatActivity() {
         val coverImg: ImageView = findViewById(R.id.movie_img)
         coverImg.setImageDrawable(layerDrawable)
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        
+        return super.onOptionsItemSelected(item)
     }
 }
